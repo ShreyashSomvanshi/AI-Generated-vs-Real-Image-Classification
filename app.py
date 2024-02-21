deb726cec27a923385b8c31f6ea0eabe70513b24
Shreyash Somvanshi <91454227+ShreyashSomvanshi@users.noreply.github.com>
2024-02-21 21:52:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,10 @@
 import streamlit as st
 import tensorflow as tf
+from tensorflow.keras.models import load_model
 import urllib.request
 from PIL import Image
 import subprocess
 import os
-
-if not os.path.isfile('model.h5'):
-    subprocess.run(['curl --output model.h5 "https://media.githubusercontent.com/media/ShreyashSomvanshi/SynthCheck/main/firstModel.h5"'], shell = True)
-    # subprocess.run(['curl --output model.h5 "https://github.com/ShreyashSomvanshi/SynthCheck-A-Synthetic-Image-Identification-using-Deep-Learning/releases/download/testing-model/AIGeneratedModel.h5"'], shell=True)
 
 st.set_page_config(
     page_title="SynthCheck",
@@ -21,48 +18,19 @@ st.title('SynthCheck: A Synthetic Image Identifier ')
 # st.image('real vs ai.jpg', width=400)
 
 
-
-# model = None
-#labels = ['real', 'fake']
-
-
-
-
-# @st.cache
-# def load_model():
-#     global model
-#     # url = 'https://github.com/ShreyashSomvanshi/SynthCheck-A-Synthetic-Image-Identification-using-Deep-Learning/releases/download/testing-model/AIGeneratedModel.h5'
-#     # filename = url.split('/')[-1]
-#     # download_model = urllib.request.urlretrieve(url, filename)
-#     model = tf.keras.models.load_model('model.h5', compile=False)
-#     # model = tf.keras.models.load_model(download_model)
-
-    #model = tf.keras.models.load_model('AIGeneratedModel.h5')
-
-
 def classify_image(file_path):
-    # if model is None:
-    # load_model()
-    model = tf.keras.models.load_model('model.h5', compile=False)
-# 
+    model = load_model('firstModel.h5')
     image = Image.open(file_path) # reading the image
-    image = image.resize((32, 32)) # resizing the image to fit the trained model
-    
+    image = image.resize((32, 32)) # resizing the image to fit the trained model   
     img = np.asarray(image) # converting it to numpy array
     img = np.expand_dims(img/255, 0)
     predictions = model.predict(img) # predicting the label
-#     label = labels[np.argmax(predictions[0])] # extracting the label with maximum probablity
-#     probab = float(round(predictions[0][np.argmax(predictions[0])]*100, 2))
     if predictions > 0.5:
         res = 'Predicted class: REAL'
     else:
         res = 'Predicted class: SYNTHETIC'
         
     return res
-#     result = {
-#         'label': label,
-#         'probablity': probab
-#     }
 
     
     
@@ -75,7 +43,5 @@ if file_uploaded is not None:
     c1.image(file_uploaded, use_column_width=True)
     c2.subheader("Classification Result")
     c2.write("The image is classified as **{}**.".format(res.title()))
-   # c2.write("The image is classified as **{}**.".format(res['label'].title()))
 
 st.button('Check', use_container_width=True) #use_container_width=True
-# st.subheader("Classification Result: ")
